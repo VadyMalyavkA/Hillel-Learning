7386cb305b73559ac66502eb6638bc4b37b05051
VadyMalyavkA <0422jimbo@gmail.com>
2024-10-18 13:14:31 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var number = 1;
+            Console.WriteLine("Hello, World!" + number);                             
+        }
+    }
+}

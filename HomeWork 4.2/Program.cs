1052ee23332732a4dcb2c0beb829a4a2c0d3b9c0
VadyMalyavkA <0422jimbo@gmail.com>
2024-10-21 17:27:51 +0300

    Add files via upload

---
+++ HomeWork 4.2/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace HomeWork_4._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter first number: ");
+                    double firstNum = double.Parse(Console.ReadLine()!);
+
+                    Console.Write("Enter char of operation ( + or - or * or / ): ");
+                    string operation = Console.ReadLine()!;
+
+                        if (operation != "+" && operation != "-" && operation != "*" && operation != "/")
+                        {
+                        Console.WriteLine("Error: Invalid input. Please enter a valid operation.\n");
+                        continue;
+                        }
+
+                    Console.Write("Enter second number: ");
+                    double secondNum = double.Parse(Console.ReadLine()!);
+
+                    var plus = firstNum + secondNum;
+                    var minus = firstNum - secondNum;
+                    var mult = firstNum * secondNum;
+                    var divide = firstNum / secondNum;
+
+                        switch (operation)
+                        {
+                            case "+":
+                                Console.WriteLine($"Your result: {plus}\n");
+                                break;
+                            case "-":
+                                Console.WriteLine($"Your result: {minus}\n");
+                                break;
+                            case "*":
+                                Console.WriteLine($"Your result: {mult}\n");
+                                break;
+                            case "/":
+                                if (secondNum != 0)
+                                    Console.WriteLine($"Your result: {divide}\n");
+                                else
+                                    Console.WriteLine("Error: Division by zero is not allowed.\n");
+                                break;
+                        }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Error: Invalid input. Please enter a valid number.\n");
+                }
+            }
+        }
+    }
+}

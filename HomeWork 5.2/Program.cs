d2560362b145daacfc504f6ccd3caf4b8a085ce0
VadyMalyavkA <0422jimbo@gmail.com>
2024-10-25 13:00:18 +0300

    Add files via upload

---
+++ HomeWork 5.2/Program.cs
@@ -0,0 +1,13 @@
+﻿while (true)
+{
+    try
+    {
+        Console.Write("Please enter you number: ");
+        int number = int.Parse(Console.ReadLine()!);
+        Console.WriteLine($"You enter: {number}\n");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"The input was not in a correct format.\nYou need write in \"INT\" format...\n");
+    }
+}

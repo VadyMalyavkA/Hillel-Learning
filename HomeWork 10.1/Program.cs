fd68b2146a62893bf18db6864010e1b7ed521866
VadyMalyavkA <0422jimbo@gmail.com>
2024-11-15 16:29:52 +0200

    Add files via upload

---
+++ HomeWork 10.1/Program.cs
@@ -0,0 +1,34 @@
+﻿while (true)
+{
+    Console.Write($"Please enter a first and last name: ");
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    string fullName = (Console.ReadLine()!).Trim();
+    Console.ResetColor();
+
+    string[] nameParts = fullName.Split(' ');
+
+    if (nameParts.Length == 2)
+    {
+        char firstNameInitial = char.ToUpper(nameParts[0][0]);
+        char lastNameInitial = char.ToUpper(nameParts[1][0]);
+
+        if (firstNameInitial == lastNameInitial)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("The last name starts with the same letter as the first name");
+            Console.ResetColor();
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The last name does not start with the same letter as the first name");
+            Console.ResetColor();
+        }
+    }
+    else
+    {
+        Console.WriteLine("The string does not contain a valid first and last name");
+    }
+    Console.WriteLine();
+}

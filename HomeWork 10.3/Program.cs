4d1d5723399d75a43ef47ea4ab9f5e0eb3d7faa5
VadyMalyavkA <0422jimbo@gmail.com>
2024-11-15 17:07:31 +0200

    Add files via upload

---
+++ HomeWork 10.3/Program.cs
@@ -0,0 +1,10 @@
+﻿while (true)
+{
+    Console.Write("Please enter the text where you need deleted \" \": ");
+
+    string input = Console.ReadLine()!;
+    string result = input.Replace(" ", "");
+
+    Console.WriteLine(result);
+    Console.WriteLine();
+}
